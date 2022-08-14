a89cb6ac24c5269157da433981412fd1552d60b0
Czang Yeob Kim <czangyeob@gmail.com>
2022-08-14 21:26:40 +0900

    modify examples/demo_analysis.py

--- examples/demo_analysis.py
+++ examples/demo_analysis.py
@@ -17,18 +17,31 @@ args = parser.parse_args()
 app = FaceAnalysis()
 app.prepare(ctx_id=args.ctx, det_size=(args.det_size,args.det_size))
 
-img = ins_get_image('t1')
+# img = ins_get_image('t1')
+# faces = app.get(img)
+# assert len(faces)==6
+# rimg = app.draw_on(img, faces)
+# cv2.imwrite("./t1_output.jpg", rimg)
+
+# # then print all-to-all face similarity
+# feats = []
+# for face in faces:
+#     feats.append(face.normed_embedding)
+# feats = np.array(feats, dtype=np.float32)
+# sims = np.dot(feats, feats.T)
+# print(sims)
+
+def get_box_xyxys(faces):
+    results = []
+    for i in range(len(faces)):
+        face = faces[i]
+        box = face.bbox.astype(np.int)
+        results.append((box[0], box[1], box[2], box[3]))
+    return results
+
+img = cv2.imread('../images/sample_GeneratedPhotosDataset-1024x613.png')
 faces = app.get(img)
-assert len(faces)==6
 rimg = app.draw_on(img, faces)
-cv2.imwrite("./t1_output.jpg", rimg)
-
-# then print all-to-all face similarity
-feats = []
-for face in faces:
-    feats.append(face.normed_embedding)
-feats = np.array(feats, dtype=np.float32)
-sims = np.dot(feats, feats.T)
-print(sims)
-
-
+cv2.imwrite("./output.jpg", rimg)
+points = get_box_xyxys(faces)
+print(points)
